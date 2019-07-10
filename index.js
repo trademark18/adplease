dbb3a94ce6e50d98b781300f3877443aa0816539
Danny Reed <danny.reed@archetypesc.com>
2019-07-10 15:18:18 -0400

    Changed CSV lib, stuff is broken

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Nightmare = require('nightmare')
 const moment = require('moment');
-const csv = require('csv-parse');
+var CsvReader = require('promised-csv');
+var reader = new CsvReader();
 require('dotenv').config(); // Currently requires: ADP_USERNAME, ADP_PASSWORD
-const parser = csv();
-const fs = require('fs');
+//const fs = require('fs');
 const nightmare = Nightmare({ show: true })
 
 
@@ -12,13 +12,16 @@ const BTN_COL_NAME_BASE = '#BtnInsertRow_';
 const IN_COL_NAME_BASE = '#INTIMEtm_';
 const OUT_COL_NAME_BASE = '#OUTTIMEtm_';
 
-var rowIndex = 0;
+var origRowIndex = 0;
 var addedRowIndex = 14; // 13 + 1
+var hasSwitchedIndex = false;
 
-var rawCsv = fs.readFileSync('FullWeekExport.csv', 'utf8');
+var rowIndex = 0;
+
+//var rawCsv = fs.readFileSync('FullWeekExport.csv', 'utf8');
 
 //Remove the quotes
-rawCsv = rawCsv.replace(/"/g, '');
+//rawCsv = rawCsv.replace(/"/g, '');
 
 // Launch the browser and navigate to the time entry screen of death and despair
 nightmare
@@ -27,74 +30,100 @@ nightmare
     .click('#UI4_ctBody_UCTodaysActivities_btnTimeSheet')
     .then(function(){
         // Read the input file
-        parser.write(rawCsv);
+        reader.read('FullWeekExport.csv')
+        .then(function(){
+            // Remove the headers
+            timeRows.shift();
+            // Reverse the records to be date ASC
+            timeRows.reverse();
+            processRecords();
+        })
     })
   //.then(nightmare.end().then(console.log))
 
 var timeRows = new Array();
 
-parser.on('readable', function(){
-    let record;
-    while(record = parser.read()){
-        timeRows.push(record);
-    }
-    
-    // Remove the headers
-    timeRows.shift();
-
-    // Reverse the records to be date ASC
-    timeRows.reverse();
-
-    processRecords();
+reader.on('row', function(data){
+    timeRows.push(data);
 })
 
-function processRecords(){
-    timeRows.forEach(x => typeInRow({
-        name: x[0],
-        start: moment(x[1]),
-        end: moment(x[2])
-    }));
+async function processRecords(){
+    for(const x of timeRows){
+        await typeInRow({
+            name: x[0],
+            start: moment(x[1]),
+            end: moment(x[2])
+        });
+        ++origRowIndex;
+        //console.log("done!");
+    }
+    
+    // timeRows.forEach(x => typeInRow({
+    //     name: x[0],
+    //     start: moment(x[1]),
+    //     end: moment(x[2])
+    // }));
  }
 
-function typeInRow(record){
+async function typeInRow(record){
     // Check the date of the current row against the current record and see if we need to add a row
-    var dateColName = DATE_COL_NAME_BASE + rowIndex;
+    var dateColName = DATE_COL_NAME_BASE + origRowIndex;
 
-    getCurrentRowDate(dateColName, function(dateRow){
+    getCurrentRowDate(dateColName, async function(dateRow){
 
         let rowDate = moment(dateRow);
         let recordDate = moment(record.start);
+        recordDate.add(7, 'day'); // TEMP
         let sameDay = rowDate.isSame(recordDate, 'day');
         
         if(!sameDay && rowDate.add(-1, 'day').isSame(recordDate, 'day')){
             // Can't put record on current row!
+            if(!hasSwitchedIndex) {
+                rowIndex = addedRowIndex;
+                console.log(`switching from origRowIndex ${origRowIndex} to addedRowIndex ${addedRowIndex}`);
+            }
+            else 
+                rowIndex = addedRowIndex++;
+
+            hasSwitchedIndex = true;
             console.log('Must add a row');
-            console.log('selector name: ', BTN_COL_NAME_BASE + (rowIndex - 1))
-            nightmare
-                .click(BTN_COL_NAME_BASE + (rowIndex - 1))
-                .then(function(){
-            });
-            return record;
+            console.log('selector name: ', BTN_COL_NAME_BASE + (origRowIndex - 1))
+            await addRow();
         }
         else {
+            if(hasSwitchedIndex) {
+                console.log(`switching from addedRowIndex ${addedRowIndex} to origRowIndex ${origRowIndex}`);
+                rowIndex = origRowIndex;
+            }
+
+            hasSwitched = false;
+            rowIndex++;
             console.log('Ready to insert values');
         }
     })
     
-    enterData(record);
+    await enterData(record);
 }
 
-function enterData(record, callback){
+async function addRow(){
     nightmare
+        .click(BTN_COL_NAME_BASE + (origRowIndex - 1))
+        .then(function(){
+            return;
+        })
+}
+
+async function enterData(record){
+    console.log('Entering on index', rowIndex);
+    await nightmare
         .click(IN_COL_NAME_BASE + rowIndex)
-        .wait(100)
+        .wait(400)
         .type(IN_COL_NAME_BASE + rowIndex, record.start.format('h:mma'))
         .wait(100)
         .click(OUT_COL_NAME_BASE + rowIndex)
-        .wait(100)
+        .wait(400)
         .type(OUT_COL_NAME_BASE + rowIndex, record.end.format('h:mma'))
-        .then(callback)
-//        .end().then(console.log);
+        .wait(100);
 }
 
 
