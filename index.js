8c7352f55a0b97e4b29639ee79464b131fa46920
Andrew Ault <andrew.ault@archetypesc.com>
2019-07-11 09:15:17 -0400

    got base good

--- index.js
+++ index.js
@@ -1,113 +1,103 @@
-const Nightmare = require('nightmare')
-const moment = require('moment');
-const csv = require('csv-parse');
-require('dotenv').config(); // Currently requires: ADP_USERNAME, ADP_PASSWORD
-const parser = csv();
-const fs = require('fs');
-const nightmare = Nightmare({ show: true })
+#!/usr/bin/env node
 
+/* dependencies */
+const csv = require('csvtojson');
+const Nightmare = require('nightmare');
+const moment = require('moment');
+const fs = require('fs');
+const path = require('path');
+
+/* env vars and args */
+/** @typedef {Object} EnvironmentVariables */
+require('dotenv').config();
+
+/* constants */
+const ISI_CLIENT_ID = 'Archetype1';
 
 const DATE_COL_NAME_BASE = '#INTIMEdt_';
 const BTN_COL_NAME_BASE = '#BtnInsertRow_';
 const IN_COL_NAME_BASE = '#INTIMEtm_';
 const OUT_COL_NAME_BASE = '#OUTTIMEtm_';
 
-var rowIndex = 0;
-var addedRowIndex = 14; // 13 + 1
+/* app */
+var app = module.exports = {
+	nightmareConfig: { show: true },
+	rowIndex: 0,
+	addedRowIndex: 14 // 13 + 1
+};
 
-var rawCsv = fs.readFileSync('FullWeekExport.csv', 'utf8');
+/** init */
+app.init = function init() {
+	//parse args
+	app.arguments = app.getArguments();
 
-//Remove the quotes
-rawCsv = rawCsv.replace(/"/g, '');
+	const csvPath = app.arguments._[0];
 
-// Launch the browser and navigate to the time entry screen of death and despair
-nightmare
-    .authentication(process.env.ADP_USERNAME, process.env.ADP_PASSWORD) // Put username and password here
-    .goto('https://ezlmportaldc1f.adp.com/ezLaborManagerNetRedirect/MAPortalStart.aspx?ISIClientID=Archetype1')
-    .click('#UI4_ctBody_UCTodaysActivities_btnTimeSheet')
-    .then(function(){
-        // Read the input file
-        parser.write(rawCsv);
-    })
-  //.then(nightmare.end().then(console.log))
+	//start up processes
+	const promises = [
+		app.initNightmarePromise(),
+		app.readCsvPromise(csvPath)
+	];
 
-var timeRows = new Array();
+	Promise.all(promises)
+		.then(([nightmareResult, csvResult]) => {
+			console.log(csvResult);
+		});
+};
 
-parser.on('readable', function(){
-    let record;
-    while(record = parser.read()){
-        timeRows.push(record);
-    }
-    
-    // Remove the headers
-    timeRows.shift();
+/** get arguments */
+app.getArguments = function getArguments() {
+	const ret = require('yargs')
+		.usage('Usage: $0 <csvFile>')
+		.demandCommand(1, 'Error: You must pass a csv file to use')
+		.argv;
 
-    // Reverse the records to be date ASC
-    timeRows.reverse();
+	return ret;
+};
 
-    processRecords();
-})
+/** start nightmare */
+app.initNightmarePromise = function initNightmarePromise() {
+	app.nightmare = Nightmare(app.nightmareConfig);
 
-function processRecords(){
-    timeRows.forEach(x => typeInRow({
-        name: x[0],
-        start: moment(x[1]),
-        end: moment(x[2])
-    }));
- }
+	const ret = app.nightmare
+		.authentication(process.env.ADP_USERNAME, process.env.ADP_PASSWORD)
+		.goto(`https://ezlmportaldc1f.adp.com/ezLaborManagerNetRedirect/MAPortalStart.aspx?ISIClientID=${ISI_CLIENT_ID}`)
+		.click('#UI4_ctBody_UCTodaysActivities_btnTimeSheet');
 
-function typeInRow(record){
-    // Check the date of the current row against the current record and see if we need to add a row
-    var dateColName = DATE_COL_NAME_BASE + rowIndex;
-
-    getCurrentRowDate(dateColName, function(dateRow){
-
-        let rowDate = moment(dateRow);
-        let recordDate = moment(record.start);
-        let sameDay = rowDate.isSame(recordDate, 'day');
-        
-        if(!sameDay && rowDate.add(-1, 'day').isSame(recordDate, 'day')){
-            // Can't put record on current row!
-            console.log('Must add a row');
-            console.log('selector name: ', BTN_COL_NAME_BASE + (rowIndex - 1))
-            nightmare
-                .click(BTN_COL_NAME_BASE + (rowIndex - 1))
-                .then(function(){
-            });
-            return record;
-        }
-        else {
-            console.log('Ready to insert values');
-        }
-    })
-    
-    enterData(record);
+	return ret;
 }
 
-function enterData(record, callback){
-    nightmare
-        .click(IN_COL_NAME_BASE + rowIndex)
-        .wait(100)
-        .type(IN_COL_NAME_BASE + rowIndex, record.start.format('h:mma'))
-        .wait(100)
-        .click(OUT_COL_NAME_BASE + rowIndex)
-        .wait(100)
-        .type(OUT_COL_NAME_BASE + rowIndex, record.end.format('h:mma'))
-        .then(callback)
-//        .end().then(console.log);
-}
+/** read csv file */
+app.readCsvPromise = function readCsvPromise(csvPath) {
+	app.csv = csv();
 
+	//turn relative csvPath into full path
+	let fullPath = csvPath;
 
-function getCurrentRowDate(dateColName, callback){
-    nightmare
-        .evaluate(dateColName => {
-            return document.querySelector(dateColName).innerText
-        }, dateColName)
-        .then(callback);
-}
+	if(!path.isAbsolute(fullPath))
+		fullPath = path.join(__dirname, fullPath);
 
+	//read file and return a promise that resolves with json
+	const ret = app.csv
+		.fromFile(csvPath)
+		.then(csvResult => {
+			const newCsvResult = csvResult.map(row => {
+				const newRow = {
+					name: row.Name,
+					start: moment(row.Start, 'M/DD/YYYY h:mm:ss A'),
+					end: moment(row.End, 'M/DD/YYYY h:mm:ss A')
+				};
 
-// TODO: Track the index of the current input (like #INTIMEtm_0)
-    // Add a row if necessary
-// Calculate the index of the new row (original rows - 1 + added rows)
-// Insert the record into the row
+				return newRow;
+			});
+
+			newCsvResult.sort((a, b) => a.valueOf() - b.valueOf());
+
+			return newCsvResult;
+		});
+
+	return ret;
+};
+
+// init :)
+app.init();
