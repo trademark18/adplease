3a10c710912ea19cf726d39d96a6e5a84081a258
Danny Reed <danny.reed@archetypesc.com>
2019-08-31 08:52:52 -0400

    File adds

---
+++ timeBot.js
@@ -0,0 +1,74 @@
+const Nightmare = require('nightmare')
+const nightmare = Nightmare();
+const moment = require('moment');
+
+const DATE_COL_NAME_BASE = '#INTIMEdt_';
+const BTN_COL_NAME_BASE = '#BtnInsertRow_';
+const IN_COL_NAME_BASE = '#INTIMEtm_';
+const OUT_COL_NAME_BASE = '#OUTTIMEtm_';
+
+var origRowIndex = 0;
+
+var nm;
+
+exports.enterTime = function(nightmareResult, entries){
+    nm = nightmareResult;
+    entries.forEach(typeInRow);
+}
+
+function typeInRow(record){
+    // Check the date of the current row against the current record and see if we need to add a row
+    var dateColName = DATE_COL_NAME_BASE + origRowIndex;
+    var rowDate
+
+    rowDatePromise(dateColName)
+    .then(function(data){
+        console.log(moment(data));
+    })
+    .then(console.log)
+    .catch(console.log);
+
+
+    // let rowDate = moment(dateRow);
+    // let recordDate = moment(record.start);
+    // recordDate.add(7, 'day'); // TEMP
+    // let sameDay = rowDate.isSame(recordDate, 'day');
+    
+    // if(!sameDay && rowDate.add(-1, 'day').isSame(recordDate, 'day')){
+    //     // Can't put record on current row!
+    //     if(!hasSwitchedIndex) {
+    //         rowIndex = addedRowIndex;
+    //         console.log(`switching from origRowIndex ${origRowIndex} to addedRowIndex ${addedRowIndex}`);
+    //     }
+    //     else 
+    //         rowIndex = addedRowIndex++;
+
+    //     hasSwitchedIndex = true;
+    //     console.log('Must add a row');
+    //     console.log('selector name: ', BTN_COL_NAME_BASE + (origRowIndex - 1))
+    //     await addRow();
+    // }
+    // else {
+    //     if(hasSwitchedIndex) {
+    //         console.log(`switching from addedRowIndex ${addedRowIndex} to origRowIndex ${origRowIndex}`);
+    //         rowIndex = origRowIndex;
+    //     }
+
+    //     hasSwitched = false;
+    //     rowIndex++;
+    //     console.log('Ready to insert values');
+    // }
+    
+    // await enterData(record);
+}
+
+var rowDatePromise = function (dateColName) {
+    return new Promise(function (resolve, reject) {
+        nm
+            .evaluate(dateColName => {
+                return document.querySelector(dateColName).innerText
+            }, dateColName)
+            .then(resolve)
+            .catch(reject);
+    });
+}
